9a7ecd2ea20e0fd8c4654f447731ba1d090e4ba6
VaultBoy9519 <illidan9519@mail.ru>
2023-05-14 12:57:20 +0300

    v1.7 от 14.05.2023. Добавлены рекомендации по отправке в СЦ

--- src/components/OptionalRecs/checkboxProps.js
+++ src/components/OptionalRecs/checkboxProps.js
@@ -1,8 +1,11 @@
 export const checkboxProps = [
+  //можно добавлять сколько угодно
   { nameTutor: "checkCookieTutor", namePupil: "checkCookiePupil", text: "Чистка cookie/кеш-файлов" },
   { nameTutor: "checkLowSpeedTutor", namePupil: "checkLowSpeedPupil", text: "Низкая скорость интернета" },
   { nameTutor: "checkBrowserTutor", namePupil: "checkBrowserPupil", text: "Нерекомендуемый браузер" },
   { nameTutor: "checkHardwareTutor", namePupil: "checkHardwarePupil", text: "Несоответствие мин.треб." },
+  { nameTutor: "serviceRecTutor", namePupil: "serviceRecPupil", text: "Отправка в сервис-центр" },
+  //всегда неизменны, т.к. подведение итогов
   { nameTutor: "checkOnpTutor", namePupil: "checkCompPupil", text: "Передача П в ОНП / У на компенс" },
   { nameTutor: "checkTrueCallTutor", namePupil: "checkTrueCallPupil", text: "Не удалось дозвониться" }
 ];
